3ed8fef43e346c13419cce7f02a59a68e8d251d2
tokyo.yu.96@gmail.com <tokyo.yu.96@gmail.com>
2020-11-11 23:51:24 +0800

    Arrival Rate is 50% done

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express'); // DO NOT DELETE
 const cors = require('cors');
 const morgan = require('morgan');
+const jsonschema = require('jsonschema');
+const jsonvalidator = new jsonschema.Validator();
 const app = express(); // DO NOT DELETE
 
 const database = require('./database');
@@ -8,11 +10,6 @@ const database = require('./database');
 app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json())
-
-app.get('/',function(req,res){
-    res.send("Test API")
-    database.testFunction()
-})
 
 /**
  * =====================================================================
@@ -51,48 +48,176 @@ app.get('/',function(req,res){
 /**
  * Company: Server Available
  */
-app.put('/company/server',async function(req,res){
-    database.serverAvailable(req.body.queue_id,function(err,result){
-        if(err == '404'){
-            console.log(404)
-            res.status(404).send({
+app.put('/company/server', function (req, res) {//Add JSON Schema Validation
 
-                error: "Queue Id "+req.body.queue_id+" Not Found",
-                code: "UNKNOWN_QUEUE"
-
-            })
-
-        }else if(err != null){
-            console.log(500)
-            res.status(500).send({
-
-                error: "Unable to establish connection with database",
-                code:  "UNEXPECTED_ERROR"
-
-            })
-            console.log(err)
-        }else{
-            if(result.rows.length != 0){
-                res.status(200).send({
-
-                    customer_id : result.rows[0].customer_id//Probably a better way to do this.
-
-                })
-            }else{
-                res.status(200).send({
-
-                    customer_id : 0
-
-                })
+    var schema = {
+        "type": "object",
+        "required": ["queue_id"],
+        "properties": {
+            "queue_id": {
+                "type": "string",
+                "maxLength": 10,
+                "minLength": 10,
+                "pattern": '^[a-zA-Z0-9]*$'
             }
-            
         }
-    })
-    
+    };
+
+    let errorStatusMsg;
+
+    let validateStatus = jsonvalidator.validate(req.body, schema)//JSON Validation
+
+    if (validateStatus.errors.length != 0) {//JSON Validation Handling
+
+        if (validateStatus.errors[0].name == 'pattern') {//Maybe use switch case for this
+            errorStatusMsg = "queueID has invalid characters"
+        } else if (validateStatus.errors[0].name == 'minLength') {
+            errorStatusMsg = "queueID is too short!"
+        } else if (validateStatus.errors[0].name == 'maxLength') {
+            errorStatusMsg = "queueID is too long!"
+        } else if (validateStatus.errors[0].name == 'type') {
+            errorStatusMsg = "queueID is a not a String"
+        } else if (validateStatus.errors[0].name == 'required') {
+            errorStatusMsg = "queueID is not in the body!"
+        }
+        res.status(400).send({//If JSON Validation returns false
+            error: errorStatusMsg,
+            code: "INVALID_JSON_BODY"
+        })
+
+    } else {
+
+        database.serverAvailable(req.body.queue_id, function (err, result) {
+            if (err == '404') {//If Q doesnt Exist
+
+                res.status(404).send({
+                    error: "Queue Id " + req.body.queue_id + " Not Found",
+                    code: "UNKNOWN_QUEUE"
+                })
+
+            } else if (err != null) {//If Other error
+
+                res.status(500).send({
+                    error: "Unable to establish connection with database",
+                    code: "UNEXPECTED_ERROR"
+                })
+
+            } else {//If Success
+                if (result.rows.length != 0) {
+                    res.status(200).send(
+                        result.rows[0]//This is already a JSON
+                    )
+                } else {
+                    res.status(200).send({
+                        customer_id: 0
+                    })
+                }
+
+            }
+
+        })
+    }
 })
 /**
  * Company: Arrival Rate
  */
+app.get('/company/arrival_rate', function (req, res) {//Add JSON Schema Validation
+    //Should we do json validation for query strings? or just normal validation
+
+    var schema = {
+        "type": "object",
+        "required": ["queue_id","from","duration"],
+        "properties": {
+            "queue_id": {
+                "type": "string",
+                "maxLength": 10,
+                "minLength": 10,
+                "pattern": '^[a-zA-Z0-9]*$'
+            },
+            "from": {
+                "type": "date-time",
+            },
+            "duration": {
+                "type": "integer",
+                "maximum": 1440,
+                "minimum": 1
+            }
+        }
+    };
+
+    let errorStatusMsg;
+
+    req.query.duration = Number(req.query.duration)//Its a Query STRING. so we need to change this to int, or number if we want to have error handling
+    
+    let validateStatus = jsonvalidator.validate(req.query, schema)//JSON Validation
+
+    if (validateStatus.errors.length != 0) {//JSON Validation Handling
+        switch (validateStatus.errors[0].property) {
+            case 'instance.queue_id':
+                if (validateStatus.errors[0].name == 'pattern') {//Should we use switch case for this
+                    errorStatusMsg = "queueID has invalid characters"
+                } else if (validateStatus.errors[0].name == 'minLength') {
+                    errorStatusMsg = "queueID is too short!"
+                } else if (validateStatus.errors[0].name == 'maxLength') {
+                    errorStatusMsg = "queueID is too long!"
+                } else if (validateStatus.errors[0].name == 'required') {
+                    errorStatusMsg = "queueID is not in the body!"
+                } else if (validateStatus.errors[0].name == 'type') {
+                    errorStatusMsg = "queueID is a not a String!"
+                }
+            break;//Add Case for from
+            case 'instance.duration':
+                if (validateStatus.errors[0].name == 'minimum'){
+                    errorStatusMsg = "duration is too low!"
+                } else if (validateStatus.errors[0].name == 'maximum') {
+                    errorStatusMsg = "duration is too high!"
+                } else if (validateStatus.errors[0].name == 'type'){
+                    errorStatusMsg = "duration is not a integer!"
+                }
+            break;
+
+        }
+        res.status(400).send({
+            error: errorStatusMsg,
+            code: "INVALID_QUERY_STRING"
+        })
+
+    }else{
+        //2018-11-13T20:20:39 08:00
+        //will return a NaN
+        //So we remove the 08:00, the GMT portion 
+        //We can also replace the " " with a GMT String or something
+        //Adding Z to the end makes it parseable
+
+        req.query.from = Date.parse((req.query.from.split(' ')[0])+'Z')/1000
+
+        database.arrivalRate(
+            req.query.queue_id,
+            req.query.from,
+            req.query.duration, 
+            function (err, result) {
+            if (err == '404') {//If Q doesnt Exist
+
+                res.status(404).send({
+                    error: "Queue Id " + req.body.queue_id + " Not Found",
+                    code: "UNKNOWN_QUEUE"
+                })
+
+            } else if (err != null) {//If Other error
+
+                res.status(500).send({
+                    error: "Unable to establish connection with database",
+                    code: "UNEXPECTED_ERROR"
+                })
+
+            } else {//If Success
+                console.log("Array"+result)
+
+            }
+
+        })
+    }
+})
 
 /**
  * ========================== CUSTOMER =========================
@@ -110,18 +235,18 @@ app.put('/company/server',async function(req,res){
  * ========================== UTILS =========================
  */
 
- 
+
 /**
  * 404
  */
 app.use(function (req, res, next) {//404
-        console.log("404")
-        res.status(404).json(
-            {
-                error: "Queue ID "+/*QUEUE ID*/1+" not found.",
-                code: "UNKNOWN_QUEUE"
-            }
-        )
+    console.log("UNKNOWN URL")
+    res.status(404).json(
+        {
+            error: "Queue ID " +/*QUEUE ID*/1 + " not found.",//What do i put here lmao
+            code: "UNKNOWN_QUEUE"
+        }
+    )
 })
 
 /**
