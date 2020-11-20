47bb8ffc79e1f693643975c929c9ef26ae8948c7
tokyo.yu.96@gmail.com <tokyo.yu.96@gmail.com>
2020-11-20 11:05:08 +0800

    Changed Listen for heroku

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ var app = require('./app.js');
 var port=3000//8081
 
 //localhost 
-var server = app.app.listen(port, function () {
+var server = app.app.listen(process.env.PORT || 3000/*For Heroku*/, function () {
     console.log(`Web App Hosted at http://localhost:${port}`,port);
 });
