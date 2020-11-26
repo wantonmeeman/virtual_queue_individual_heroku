552da130fdd8e619f781ada68364b532ead11ace
Alastair <alastairtan.19@ichat.sp.edu.sg>
2020-11-27 01:01:52 +0800

    ADES Group 4 CA1 Submission

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 var app = require('./app.js');
-
-
-var port=3000 //8081
+var port = 3000 //8081
 
 //localhost 
 var server = app.app.listen(port, function () {
-    console.log(`Web App Hosted at http://localhost:${port}`,port);
+    console.log(`Web App Hosted at http://localhost:${port}`, port);
 });
