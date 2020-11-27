b89eb7ca7f4abebe2e52991ba60e31966a980314
tokyo.yu.96@gmail.com <tokyo.yu.96@gmail.com>
2020-11-27 08:24:50 +0800

    Added Changed to make heroku work

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var app = require('./app.js');
 var port = 3000 //8081
 
 //localhost 
-var server = app.app.listen(port, function () {
+var server = app.app.listen(process.env.PORT || port, function () {
     console.log(`Web App Hosted at http://localhost:${port}`, port);
 });
